d87629b88c292c0f96c084fc2ac9a1268586353c
brandon <brandonochoa202030023@cunoc.edu.gt>
2025-04-18 21:50:30 -0600

    Obteniendo usuarios

---
+++ frontend/AppSalonBelleza/src/app/interfaces/user-cliente.ts
@@ -0,0 +1,2 @@
+export interface UserCliente {
+}
--- frontend/AppSalonBelleza/src/app/services/register-service.service.ts
+++ frontend/AppSalonBelleza/src/app/services/register-service.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Administrador } from '../interfaces/administrador';
+import { Observable } from 'rxjs';
+import { UserMarketing } from '../interfaces/user-marketing';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +16,8 @@ export class RegisterServiceService {
   crearAdministrador(admin: Administrador){
     return this.http.post<Administrador>(`${this.API_URL}/RegistrarAdministrador`, admin);
   }
-} 
+
+  obtenerUsuariosMarketing(): Observable<UserMarketing[]>{
+    return this.http.get<UserMarketing[]>(`${this.API_URL}/RegistrarAdministrador`);
+  }
+}
